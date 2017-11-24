9fc6c2b3fc5c168ad341a3d23c6efbc65ad4c3f4
Xavier Simó <xavier.simo@xing.com>
2017-11-24 10:15:51 +0100

    Add version with goroutines
    Surprisingly it lasts 3X!!

--- day5/chans.go
+++ day5/chans.go
@@ -18,20 +18,38 @@ func main() {
 	start := time.Now()
 	defer func() { fmt.Printf("It took: %v\n", time.Since(start)) }()
 
-	for i := 0; len(pass) < 8; i++ {
-		hashInput(i)
+	done := make(chan bool)
+	res := make(chan rune)
+
+	var i int
+	for i = 0; i < 10; i++ {
+		go hashInput(i, res, done)
 	}
+
+	for len(pass) < 8 {
+		select {
+		case <-done:
+		case r := <-res:
+			fmt.Println("found one:", r)
+			pass = append(pass, r)
+		}
+
+		i++
+		go hashInput(i, res, done)
+	}
+
 	fmt.Println(string(pass))
 }
 
-func hashInput(i int) {
+func hashInput(i int, res chan rune, done chan bool) {
 	h := md5.New()
 	io.WriteString(h, input+strconv.Itoa(i))
 	src := h.Sum(nil)
 	dst := make([]byte, hex.EncodedLen(len(src)))
 	hex.Encode(dst, src)
 	if strings.HasPrefix(string(dst), "00000") {
-		pass = append(pass, rune(dst[5]))
-		fmt.Println("found one: ", dst, string(pass), i)
+		res <- rune(dst[5])
+		return
 	}
+	done <- true
 }
