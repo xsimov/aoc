604ef371d6c29c7858872bc66cd9b813be1a5773
Xavier Simó <xavier.simo@xing.com>
2018-03-24 23:07:49 +0100

    Refactor some of the Instruction tests

--- 8th/instruction_test.go
+++ 8th/instruction_test.go
@@ -1,20 +1,42 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestInstructionBuilderWithInvalidCommand(t *testing.T) {
+	_, err := newInstructionFromString("gibberish 3x2")
+	if err == nil {
+		t.Fatalf("expected error not to be nil, but it was")
+	}
+	expectedError := `instruction first argument must be rect or rotate, got "gibberish" instead (from "gibberish 3x2")`
+	if err.Error() != expectedError {
+		t.Fatalf("error (%q) was different from expected error: %q", err.Error(), expectedError)
+	}
+}
 
 func TestInstructionBuilderWithRectangle(t *testing.T) {
+	i, _ := newInstructionFromString("rect 3x2")
+	_, ok := i.(rectInstruction)
+	if !ok {
+		t.Fatalf("expected %v(%T) to be of type rectInstruction", i, i)
+	}
+}
+
+func TestRectInstructionFieldsAreAssigned(t *testing.T) {
 	i, err := newInstructionFromString("rect 3x2")
+	r := i.(rectInstruction)
 	if err != nil {
 		t.Fatalf("expected error to be nil, but was: %v", err)
 	}
-	if i.command != "rect" {
-		t.Fatalf("expected command to be 'rect' and it was %q", i.command)
+	if r.Command() != "rect" {
+		t.Fatalf("expected command to be 'rect' and it was %q", r.Command())
 	}
-	if i.x != 3 {
-		t.Fatalf("expected x to be 3 and it was %v", i.x)
+	if r.X != 3 {
+		t.Fatalf("expected x to be 3 and it was %v", r.X)
 	}
-	if i.y != 2 {
-		t.Fatalf("expected y to be 2 and it was %v", i.y)
+	if r.Y != 2 {
+		t.Fatalf("expected y to be 2 and it was %v", r.Y)
 	}
 }
 
@@ -23,6 +45,10 @@ func TestInstructionBuilderWithRectangleIncorrectX(t *testing.T) {
 	if err == nil {
 		t.Fatalf("expected error not to be nil but it was: %v", err)
 	}
+	expectedError := `instruction first argument must be rect or rotate, got "gibberish" instead (from "gibberish 3x2")`
+	if err.Error() != expectedError {
+		t.Fatalf("error (%q) was different from expected error: %q", err.Error(), expectedError)
+	}
 }
 
 func TestInstructionBuilderWithRectangleIncorrectY(t *testing.T) {
@@ -30,20 +56,25 @@ func TestInstructionBuilderWithRectangleIncorrectY(t *testing.T) {
 	if err == nil {
 		t.Fatalf("expected error not to be nil but it was: %v", err)
 	}
+	expectedError := `instruction first argument must be rect or rotate, got "gibberish" instead (from "gibberish 3x2")`
+	if err.Error() != expectedError {
+		t.Fatalf("error (%q) was different from expected error: %q", err.Error(), expectedError)
+	}
 }
 
 func TestInstructionBuilderWithRotate(t *testing.T) {
-	i, err := newInstructionFromString("rect 3x2")
+	i, err := newInstructionFromString("rotate row y=1 by 5")
+	r := i.(rotateInstruction)
 	if err != nil {
 		t.Fatalf("expected error to be nil, but was: %v", err)
 	}
-	if i.command != "rect" {
-		t.Fatalf("expected command to be 'rect' and it was %q", i.command)
+	if r.Command() != "rotate" {
+		t.Fatalf("expected command to be 'rotate' and it was %q", r.Command())
 	}
-	if i.x != 3 {
-		t.Fatalf("expected x to be 3 and it was %v", i.x)
+	if r.Index != 1 {
+		t.Fatalf("expected index to be 1 and it was %v", r.Index)
 	}
-	if i.y != 2 {
-		t.Fatalf("expected y to be 2 and it was %v", i.y)
+	if r.Direction != "row" {
+		t.Fatalf("expected direction to be row and it was %v", r.Direction)
 	}
 }
